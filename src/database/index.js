948986ba28944798b59e43e1babd2377a44deba6
Pedro Giampietro <pedrogiampietro@hotmail.com>
2020-11-29 01:14:46 -0300

    backup to format pc.

--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost/noderest', { useMongoClient: true })
+mongoose.connect('mongodb+srv://pedrogiampietro:899681ph@cluster0.03uyz.gcp.mongodb.net/studydb?retryWrites=true&w=majority', { useUnifiedTopology: true, useNewUrlParser: true })
 mongoose.Promise = global.Promise
+
 
 module.exports = mongoose
